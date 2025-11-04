debf13d6047c62340258654c57ee07fcb73ebb1e
Vamsi_0 <iamvamsi0@gmail.com>
2025-11-04 16:57:37 +0530

    Intial zCommit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center items-center min-h-screen backdrop-blur bg-black/25 ">
-      <div className="bg-white/10 rounded-xl m-4">
-        {/* <label htmlFor="name"></label> */}
-        <input className="placeholder-gray-150  bg-transparent focus:outline-none  text-white p-4 " type="text" placeholder="Enter you name " />
-      </div>
-    
+    // <div className="flex flex-col justify-center items-center min-h-screen backdrop-blur bg-black/25 ">
+    //   <div className="bg-white/10 rounded-xl m-4">
+    //     {/* <label htmlFor="name"></label> */}
+    //     <input className="placeholder-gray-150  bg-transparent focus:outline-none  text-white p-4 " type="text" placeholder="Enter you name " />
+    //   </div>
+    // </div>
+    <div className=" flex min-h-screen max-w-full ">
+      Hello world 
     </div>
+    
   );
 }
